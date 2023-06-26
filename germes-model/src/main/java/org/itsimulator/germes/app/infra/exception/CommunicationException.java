008dbce40391ab75775f70fe64b9cefc3f185ba5
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-06-26 18:18:19 +0300

    add exceptions

---
+++ germes-model/src/main/java/org/itsimulator/germes/app/infra/exception/CommunicationException.java
@@ -0,0 +1,21 @@
+package org.itsimulator.germes.app.infra.exception;
+
+import org.itsimulator.germes.app.infra.exception.base.AppException;
+
+/**
+ * Signals about exception cases in the work of external services and API
+ * @author maxim
+ */
+
+public class CommunicationException extends AppException {
+	
+	private static final long serialVersionUID = -2850898723336164866L;
+	
+	public CommunicationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	public CommunicationException(String message) {
+		super(message);
+	}
+
+}
---
+++ germes-model/src/main/java/org/itsimulator/germes/app/infra/exception/PersistenceException.java
@@ -0,0 +1,21 @@
+package org.itsimulator.germes.app.infra.exception;
+
+import org.itsimulator.germes.app.infra.exception.base.AppException;
+
+/*
+ * Signals about data access layer unexpected situations
+ * @author maxim
+ */
+
+public class PersistenceException extends AppException {
+	
+	private static final long serialVersionUID = -7889716045779735512L;
+	
+	public PersistenceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	public PersistenceException(String message) {
+		super(message);
+	}
+
+}
