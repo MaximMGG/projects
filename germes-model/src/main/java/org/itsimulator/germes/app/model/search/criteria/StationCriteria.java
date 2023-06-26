a1d4f58092aadc699af182fc01161a428d085d9d
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-06-26 17:57:59 +0300

    add some criterias

---
+++ germes-model/src/main/java/org/itsimulator/germes/app/model/search/criteria/StationCriteria.java
@@ -0,0 +1,59 @@
+package org.itsimulator.germes.app.model.search.criteria;
+
+import org.itsimulator.germes.app.model.entity.transport.TransportType;
+
+/**
+ * Filtering criteria for search stations operation
+ * @author maxim
+ */
+
+public class StationCriteria {
+	/**
+	 * City's name
+	 */
+	private String name;
+	
+	private TransportType transportType;
+	
+	/**
+	 * Station's address: street, zipCode, Building number
+	 */
+	private String address;
+	
+	/**
+	 * Return filter criteria to search stations that contains specified name parameter
+	 * @param name
+	 * @return
+	 */
+	public static StationCriteria byName(String name) {
+		return new StationCriteria(name);
+	}
+	public StationCriteria() {}
+	
+	private StationCriteria(final String name) {
+		this.name = name;
+	}
+	
+	public StationCriteria(final TransportType transportType) {
+		this.transportType = transportType;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public TransportType getTransportType() {
+		return transportType;
+	}
+	public void setTransportType(TransportType transportType) {
+		this.transportType = transportType;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
